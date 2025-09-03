bf6efc8b4621d3ad05b195ec1ea7614d2787d636
Sahil Kumar <sahilkumarofficial05@gmail.com>
2025-09-03 19:20:00 +0530

    feat:layout added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 export default function Home() {
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			Let&apos;s start
-		</div>
+		<main className="bg-[#F5F8FF] min-h-screen flex items-center justify-center">
+			<Card className="content h-[calc(100vh_-_100px)] border">
+				<CardContent className="grid grid-cols-2 grid-rows-2 h-full lg:gap-6 md:gap-4 gap-2">
+					<Card className="row-span-2"></Card>
+					<Card className="">
+						<CardContent className="">
+							<div className="">
+								<p className="text-primary">Task Status</p>
+							</div>
+						</CardContent>
+					</Card>
+					<Card className="">hello3</Card>
+				</CardContent>
+			</Card>
+		</main>
 	);
 }
