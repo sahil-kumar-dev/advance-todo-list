f4c8da27b2ca298e2819b0c92a436b9295f1c76f
Sahil Kumar <sahilkumarofficial05@gmail.com>
2025-09-03 22:36:41 +0530

    feat:added form validation using react hook form

--- schema/index.ts
+++ schema/index.ts
@@ -1,16 +1,11 @@
 import { z } from 'zod'
 
 export const addTodoSchema = z.object({
-    title: z.string().min(1, { message: 'Title is required.' }),
-    description: z.string().min(1, { message: 'Description is required.' }),
-    date: z.preprocess(
-        (arg) => (typeof arg === 'string' || arg instanceof Date ? new Date(arg) : arg),
-        z.date().refine(
-            (date) => date > new Date(),
-            { message: 'Date must be in the future.' }
-        )
-    ),
+    title: z.string().min(6, { message: 'Title must be at least 6 characters.' }),
+    description: z.string().min(10, { message: 'Description must be at least 10 characters.' }),
+    startDate: z.date().min(1, { message: 'Date is required.' }),
     category: z.string().min(1, { message: 'Category is required.' }),
+    endDate: z.date({ message: 'End date is required.' })
 })
 
 export type AddTodoSchema = z.infer<typeof addTodoSchema>
